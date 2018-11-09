6a7023437d5ffb2a87c9dc3752c6ebacd6e50527
Onat <egeonat@gmail.com>
2018-11-09 20:13:50 +0300

    Add roll functionality to GameFrame

--- src/ui/GameFrame.java
+++ src/ui/GameFrame.java
@@ -8,9 +8,18 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import domain.controller.GameController;
+import domain.model.dice.Cup;
+import domain.model.dice.faceValue;
+
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.BoxLayout;
@@ -22,11 +31,12 @@ import javax.swing.SwingConstants;
 public class GameFrame extends JFrame {
 
 	private JPanel contentPane;
-
+	private GameController gameController;
 	/**
 	 * Create the frame.
 	 */
 	public GameFrame() {
+		gameController = GameController.getInstance();
 		setBounds(new Rectangle(0, 0, 1500, 1000));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,17 +65,34 @@ public class GameFrame extends JFrame {
 			
 			JButton rollButton = new JButton("Roll");
 			rollButton.setBounds(105, 172, 300, 40);
+			
 			panel.add(rollButton);
 			
 			JButton moveButton = new JButton("Move");
 			moveButton.setBounds(105, 225, 300, 40);
 			panel.add(moveButton);
 			
-			JLabel lblYouRolledX = new JLabel("You rolled: X X X");
-			lblYouRolledX.setHorizontalAlignment(SwingConstants.CENTER);
-			lblYouRolledX.setFont(new Font("Tahoma", Font.PLAIN, 20));
-			lblYouRolledX.setBounds(105, 278, 300, 40);
-			panel.add(lblYouRolledX);
+			JLabel rollLabel = new JLabel("You rolled: X X X");
+			rollLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			rollLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			rollLabel.setBounds(105, 278, 300, 40);
+			panel.add(rollLabel);
+			
+			rollButton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+					gameController.roll();
+					Cup cup = Cup.getInstance();
+					List<faceValue> faceValList = cup.getFaceValues();
+					String str = "=>";
+					for(int i=0; i<3;i++)
+						str += " " + faceValList.get(i).toString();
+					rollLabel.setText(str);
+				}
+			});
 
 		} catch (IOException e) {
 			e.printStackTrace();
