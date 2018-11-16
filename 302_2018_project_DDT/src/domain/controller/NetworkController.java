eade8d734c6a65ae45cd1562d3a359049d1619cc
Waris <wgill18@ku.edu.tr>
2018-11-16 11:14:04 +0300

    extra folder deleted

--- 302_2018_project_DDT/src/domain/controller/NetworkController.java
+++
@@ -1,9 +0,0 @@
-package domain.controller;
-
-import java.util.HashMap;
-
-public interface NetworkController {
-	public void publishNetworkEvent(HashMap<String, String>	map);
-	public void addNetworkControllerListener(NetworkControllerListener listener);
-	public void sendMessageToPlayers(HashMap<String, String> map);
-}
--- 302_2018_project_DDT/src/domain/controller/NetworkControllerListener.java
+++
@@ -1,7 +0,0 @@
-package domain.controller;
-
-import java.util.HashMap;
-
-public interface NetworkControllerListener {
-	public void onNetworkEvent(NetworkController source, HashMap<String, String> map);
-}
