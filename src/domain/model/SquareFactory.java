afc23473891d06a37de696a169044660b0107089
baris <baris@192.168.1.23>
2018-11-18 01:42:09 +0200

    update for square factory
    Roll3
    Community Chest
    Free parking
    Chance Action
    Transit
    Jail

--- src/domain/model/SquareFactory.java
+++ src/domain/model/SquareFactory.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import domain.model.specialSquares.ChanceAction;
 import domain.model.specialSquares.CommunityChest;
 import domain.model.specialSquares.FreeParking;
 import domain.model.specialSquares.IncomeTax;
+import domain.model.specialSquares.Jail;
+import domain.model.specialSquares.RollThree;
 import domain.model.specialSquares.TransitStation;
 import domain.model.specialSquares.payCorners.Go;
 
@@ -26,10 +29,19 @@ public class SquareFactory {
 	}
 	
 	public Square createSquare(String name) {
-		if(name.equals("Mediterranean")) {
-			return new Street("Mediterranean Avenue", 60,"", "Purple", deedFactory.createDeed(name));
+		if(name.equals("Roll3")) {
+			return new RollThree("Roll3", "Draw a roll three card.");
+		}else if(name.equals("Community Chest")){
+			return new CommunityChest("Community Chest", "Draw a community chest card.");
+		}else if(name.equals("")){
+			return new FreeParking("freeParking", "You can stay here for free!!!");
+		}else if(name.equals("Chance")){
+			return new ChanceAction("Chance", "Draw a chance action card.");
+		}else if(name.equals("Transit")){
+			return new TransitStation("Transit", "You can transfer to another layer from here.");
+		}else if(name.equals("Jail")){
+			return new Jail("Jail", "Stay in or visit the jail.");
+		}else
+			return new FreeParking("freeParking", "You can stay here for free!!!");
 		}
-	else return null;
 	}
-
-}
