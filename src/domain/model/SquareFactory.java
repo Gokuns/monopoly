df38a2d736d515181fbc8cb2abb333af6b2ee118
Goko <gunsal15@ku.edu.tr>
2018-11-18 02:20:11 +0300

    Started on SquareFactory

--- src/domain/model/SquareFactory.java
+++ src/domain/model/SquareFactory.java
@@ -12,9 +12,10 @@ import domain.model.specialSquares.payCorners.Go;
 
 public class SquareFactory {
 	private static SquareFactory squareFactory;
+	private DeedFactory deedFactory;
 	
 	private SquareFactory() {
-		
+		this.deedFactory = DeedFactory.getInstance();
 	}
 	
 	public static synchronized SquareFactory getInstance() {
@@ -24,6 +25,11 @@ public class SquareFactory {
 		return squareFactory;
 	}
 	
-	
+	public Square createSquare(String name) {
+		if(name.equals("Mediterranean")) {
+			return new Street("Mediterranean Avenue", 60,"", "Purple", deedFactory.createDeed(name));
+		}
+	else return null;
+	}
 
 }
