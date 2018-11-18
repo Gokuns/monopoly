ca1245472fbd11571a466ef455755ea34c1d47e8
gunsal15 <gunsal15@ku.edu.tr>
2018-11-18 15:30:15 +0300

    Update src/domain/model/Company.java

--- src/domain/model/Company.java
+++ src/domain/model/Company.java
@@ -4,7 +4,6 @@ public class Company extends Property{
 
 	public Company(String name, int price, String description, Deed deed, int layer, int number) {
 		super(name, price, description, deed, layer, number);
-		// TODO Auto-generated constructor stub
 	}
 
 }
