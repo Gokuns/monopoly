8528d7a4efa430c6bf3ffc3a5f095c894b589877
Gokalp <gunsal15@ku.edu.tr>
2018-12-06 20:33:47 +0300

    birthday gift special square

--- src/domain/model/squares/specialSquares/BirthdayGift.java
+++ src/domain/model/squares/specialSquares/BirthdayGift.java
@@ -1,5 +1,8 @@
 package domain.model.squares.specialSquares;
 
+import java.util.ArrayList;
+
+import domain.model.GameState;
 import domain.model.players.Player;
 import domain.model.squares.SpecialSquare;
 
@@ -12,8 +15,19 @@ public class BirthdayGift extends SpecialSquare {
 
 	@Override
 	public String action(Player p) {
-		return null;
+		GameState game= GameState.getInstance();
+		ArrayList<Player> list = game.getOrderedPlayerList();
+		for (Player player : list) {
+			if(player!=p) {
+			player.setBalance(player.getBalance()-50);
+			}
+		}
+		int present = (list.size()-1)*50;
+		p.setBalance(p.getBalance()+present);
+		return p.getName() + " got a total amount of " + present ;
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
