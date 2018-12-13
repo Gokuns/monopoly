29186abecc8494e50b693d9019d13d1f6603e943
Gokalp <gunsal15@ku.edu.tr>
2018-12-13 12:11:04 +0300

    JavaDoc on GameState

--- src/domain/model/GameState.java
+++ src/domain/model/GameState.java
@@ -7,6 +7,12 @@ import domain.model.dice.Cup;
 import domain.model.players.Player;
 import domain.model.squares.Square;
 
+/**
+ * The Class that holds the information of the players and being the subject.
+ * This is the object that will be serialized.
+ * @author Goko
+ *
+ */
 public class GameState {
 	private static GameState game;
 	private Player currentPlayer;
@@ -21,8 +27,15 @@ public class GameState {
 	private ArrayList<GameStateListener> networkListeners = new ArrayList<GameStateListener>();
 	private ArrayList<GameStateListener> UIListeners = new ArrayList<GameStateListener>();
 	
+	/**
+	 * Constructor
+	 */
 	private GameState() {}
 	
+	/**
+	 * Singleton getInstance method
+	 * @return gameState object, creates new if not created bofore.
+	 */
 	public static synchronized GameState getInstance() {
 		if(game == null) {
 			game = new GameState();
@@ -30,26 +43,45 @@ public class GameState {
 		return game;
 	}
 	
+	/** adds the listener to network listeners list.
+	 * @param listener: observer that observers this class.
+	 */
 	public void addNetworkListener(GameStateListener listener) {
 		networkListeners.add(listener);
 	}
 	
+	/**
+	 * adds the listener to UI listeners list.
+	 * @param listener: observer that observers this class.
+	 */
 	public void addUIListener(GameStateListener listener) {
 		UIListeners.add(listener);
 	}
 	
+	/**
+	 * send the  listeners from network classes the command to update their states.
+	 * @param map: hasmap, containing the info
+	 */
 	public void publishToNetworkListeners(HashMap<String, String> map) {
 		for(GameStateListener listener : networkListeners) {
 			listener.update(this, map);
 		}
 	}
 	
+	/**
+	 * send the listeners from UI classes the command to update their states.
+	 * @param map: hasmap, containing the info
+	 */
 	public void publishToUIListeners(HashMap<String, String> map) {
 		for(GameStateListener listener : UIListeners) {
 			listener.update(this, map);
 		}
 	}
 	
+	/**
+	 * Changes the currently selected player to the next one in the list
+	 * @return Player that is next in the line.
+	 */
 	public Player getNextPlayer() {
 		
 		int nPlayers = orderedPlayerList.size();
@@ -72,10 +104,18 @@ public class GameState {
 		return nextPlayer;
 	}
 	
+	/**
+	 * get currntly selected players squares
+	 * @return Square
+	 */
 	public Square getPlayerCurrentSquare() {
 		return currentPlayer.getPiece().getCurrentSquare();
 	}
-
+	
+	/**
+	 * getter of the current player.
+	 * @return Player.
+	 */
 	public Player getCurrentPlayer() {
 		return currentPlayer;
 	}
@@ -84,15 +124,27 @@ public class GameState {
 // 	public int getnPlayers() {
 // 		return nPlayers;
 // 	}
-
+	
+	/**
+	 * The list of players in the game
+	 * @return List of Players
+	 */
 	public ArrayList<Player> getPlayerList() {
 		return playerList;
 	}
 
+	/**
+	 * The ordered version of the Player list.
+	 * @return List of PLayers
+	 */
 	public ArrayList<Player> getOrderedPlayerList() {
 		return orderedPlayerList;
 	}
 
+	/**
+	 * setter of the current player field.
+	 * @param currentPlayer : different player.
+	 */
 	public void setCurrentPlayer(Player currentPlayer) {
 		this.currentPlayer = currentPlayer;
 	}
@@ -102,14 +154,27 @@ public class GameState {
 // 		this.nPlayers = nPlayers;
 // 	}
 
+	/**
+	 * Setter of the player list
+	 * @param playerList : List of Players
+	 */
 	public void setPlayerList(ArrayList<Player> playerList) {
 		this.playerList = playerList;
 	}
 
+	/**
+	 * setter of the ordered player list
+	 * @param orderedPlayerList : List of Players
+	 */
 	public void setOrderedPlayerList(ArrayList<Player> orderedPlayerList) {
 		this.orderedPlayerList = orderedPlayerList;
 	}
-
+	
+	/**
+	 * Adds the player the Player list
+	 * @param username : username of the new player
+	 * @param ID : Id of the new player
+	 */
 	public void addPlayer(String username, int ID) {
 		Player p = new Player(username, ID);
 		playerList.add(p);
